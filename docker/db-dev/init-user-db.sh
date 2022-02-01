5555cb8aaddd7a5ffa1820dfb50f0fdd3eaaff6b
Mary Chester-Kadwell <mec31@cam.ac.uk>
2022-02-01 10:52:19 +0000

    Start dockerise non-Django services for development

---
+++ docker/db-dev/init-user-db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER muya WITH PASSWORD '$MUYA_PASSWORD' CREATEDB;
+    CREATE DATABASE muya;
+    GRANT ALL PRIVILEGES ON DATABASE muya TO muya;
+EOSQL
