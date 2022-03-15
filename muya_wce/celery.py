1f2c861237c79eb4af9de7417c9e23711ab4602a
Mary Chester-Kadwell <mec31@cam.ac.uk>
2022-03-15 10:32:55 +0000

    Celery tasks not completed are cancelled and returned to queue to cope with connection loss See: https://docs.celeryproject.org/en/stable/userguide/configuration.html#worker-cancel-long-running-tasks-on-connection-loss

--- muya_wce/celery.py
+++ muya_wce/celery.py
@@ -5,4 +5,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muya_wce.settings')
 
 app = Celery('muya_wce')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+# Tasks which have not been completed are cancelled and their execution is terminated; task is returned to queue
+# See: https://docs.celeryproject.org/en/stable/userguide/configuration.html#worker-cancel-long-running-tasks-on-connection-loss
+app.conf.worker_cancel_long_running_tasks_on_connection_loss = True
 app.autodiscover_tasks()
