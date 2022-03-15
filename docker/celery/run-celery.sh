c390f817b6eedaa8ee7fcad1dc0a02217338e682
Mary Chester-Kadwell <mec31@cam.ac.uk>
2022-03-15 10:30:19 +0000

    Prepare docker setup for production deployment

--- docker/celery/run-celery.sh
+++ docker/celery/run-celery.sh
@@ -3,5 +3,5 @@
 if [[ "${MUYA_DEVSERVER:-false}" == "true" ]]; then
     exec celery --app muya_wce worker --loglevel INFO
 else
-    exec celery --app muya_wce worker --loglevel INFO --pool gevent --concurrency 500
+    exec celery --app muya_wce worker --loglevel INFO --pool gevent --concurrency 500 --uid muya
 fi
